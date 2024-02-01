371e09452ebd1e926278e4317ff27f618dee28a2
Lucas Montezuma <lucas.montezumaa@gmail.com>
2024-02-01 18:23:33 -0300

    first commit

---
+++ teste.js
@@ -0,0 +1,25 @@
+const nome =""
+const nota = 0
+
+class Aluno {
+    nome=nome;
+    nota=nota;
+
+    constructor(nomePessoa,notaPessoa){
+        this.nome=nomePessoa
+        this.nota=notaPessoa
+    }
+}
+
+const joao= new Aluno("João",6)
+const pedro=new Aluno("Pedro",4)
+const maria=new Aluno("Maria",7)
+
+const arrayalunos = [joao,pedro,maria]
+
+console.log(arrayalunos)
+
+arrayalunos.forEach(aluno =>{
+    if(aluno.nota >= 6)
+    console.log(aluno)
+})
